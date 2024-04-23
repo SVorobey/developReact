da79038cd99379671daa9fc0165dddfaf5590c76
Stanislav Vorobey <SVorobei@egartech.ru>
2024-04-23 17:58:22 +0300

    idk

--- src/features/calendar/calendarCellsWeek/index.tsx
+++ src/features/calendar/calendarCellsWeek/index.tsx
@@ -1,7 +1,7 @@
 import moment from "moment";
 import { WeekDaysWrapper } from "../calendarCellsMonth/styles";
-import { DayInHeader, CellsWrapper } from "./styles";
-import { daysOfWeek } from "@app/momentConfig";
+import { DayInHeader, CellsWrapper, WeekWrapper, Cells } from "./styles";
+import { daysOfWeek, time } from "@app/momentConfig";
 import { Number, DayName } from "./styles";
 
 
@@ -11,13 +11,18 @@ type Props = {
 
 export const CalendarCellsWeek:React.FC<Props> = ({fulldate}) => {
     const day = moment(fulldate).startOf('week');
+    const Date = '2024-03-24';
+    console.log(moment(Date).startOf('week').unix());
     const calendarDays = [...Array(7)].map(() => day.add(1, 'day').clone());
   return (
     <CellsWrapper>
       <WeekDaysWrapper />
       {
         calendarDays.map((item, index) => (
-          <WeekDaysWrapper>
+          <WeekWrapper
+            isweekend={(item.day() === 6 || item.day() === 0) ? 'true' : 'false'}
+            isCurrentMonth={item.month() === moment(fulldate).month() ? 'true' : 'false'}
+          >
               <DayInHeader>
                 <Number>
                   {item.format('D')}
@@ -26,7 +31,14 @@ export const CalendarCellsWeek:React.FC<Props> = ({fulldate}) => {
                   {daysOfWeek[index].toUpperCase()}
                 </DayName>
               </DayInHeader>
-          </WeekDaysWrapper>
+          </WeekWrapper>
+        ))
+      }
+      {
+        time.map((item) => (
+          <Cells>
+            {item}
+          </Cells>
         ))
       }
     </CellsWrapper>
--- src/features/calendar/calendarCellsWeek/styles.tsx
+++ src/features/calendar/calendarCellsWeek/styles.tsx
@@ -1,4 +1,10 @@
 import styled from "styled-components";
+import { WeekDaysWrapper } from "../calendarCellsMonth/styles";
+
+type Props = {
+    isweekend?: string,
+    isCurrentMonth?: string,
+}
 
 export const Number = styled.p`
     font-size: 20px;
@@ -20,9 +26,22 @@ export const DayInHeader = styled.div`
 export const CellsWrapper = styled.div`
     display: grid;
     grid-template-columns: repeat(8, 1fr);
+    grid-gap: 4px;
 `;
 
 export const TimeDiv = styled.div`
     color: #000;
     font-size: 20px
+`;
+
+export const Cells = styled.div`
+    min-width: 140px;
+    min-height: 100px;
+    box-shadow: rgba(0, 0, 0, 0.1) 0px 1px 3px 0px, rgba(0, 0, 0, 0.06) 0px 1px 2px 0px;
+    border-radius: 5px;
+`;
+
+export const WeekWrapper = styled(WeekDaysWrapper)<Props>`
+    background: ${props => props.isweekend === 'true' ? '#F9F9F9' : '#FFF'};
+    color: ${props => props.isCurrentMonth === 'true' ? '#000' : '#A9A9A9'};
 `;
